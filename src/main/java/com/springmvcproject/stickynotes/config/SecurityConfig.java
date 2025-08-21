03606cdc7af57e4f4f10ed53647c72739e1f0280
kamal shekho <k.shekho@yahoo.com>
2025-08-21 16:07:49 +0200

    create Security Config

---
+++ src/main/java/com/springmvcproject/stickynotes/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.springmvcproject.stickynotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(authRequest -> {
+            authRequest.requestMatchers("/about", "css/**").permitAll();
+            authRequest.requestMatchers("/", "/note/**", "/my-notes/**").authenticated();
+            authRequest.requestMatchers("/users/**").hasRole("ADMIN");
+            authRequest.anyRequest().permitAll();
+        });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+}
--- src/main/java/com/springmvcproject/stickynotes/filters/AuditFilter.java
+++ src/main/java/com/springmvcproject/stickynotes/filters/AuditFilter.java
@@ -1,19 +1,23 @@
 package com.springmvcproject.stickynotes.filters;
 
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @Component
-public class AuditFilter extends OncePerRequestFilter {
+public class AuditFilter extends OncePerRequestFilter{
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Request path : " + request.getServletPath());
-        doFilter(request, response, filterChain);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("Request Path:" + request.getServletPath());
+     doFilter(request, response, filterChain);
     }
+    
 }
