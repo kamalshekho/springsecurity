d6aa16cb18450574afb2e721369ddadf8689ec1e
Kamal Shekho <kamal.shekho@transwaggon.group>
2025-08-28 13:24:56 +0200

    add oauth2

--- src/main/java/com/springmvcproject/stickynotes/config/SecurityConfig.java
+++ src/main/java/com/springmvcproject/stickynotes/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,34 +16,58 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import lombok.RequiredArgsConstructor;
 
 @Configuration
-@RequiredArgsConstructor(onConstructor_ = { @Autowired })
+@EnableWebSecurity
+// @RequiredArgsConstructor(onConstructor_ = { @Autowired })
 public class SecurityConfig {
 
-    private final TokenProvider tokenProvider;
-
-@Bean
- SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable())
-        .sessionManagement(session -> 
-            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        )
-        .addFilterBefore(new JWTFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/users/register", "/api/users/create", "/api/users/login").permitAll()
-            .anyRequest().authenticated()
-        );
-
-    return http.build();
-}
-
     @Bean
-    PasswordEncoder defaultPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .disable())
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest()
+                        .authenticated());
+       http
+                .oauth2ResourceServer(server -> server
+                        .jwt());
+
+        http
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
     }
 
-    @Bean
-    AuthenticationManager defaultAuthenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
+
+
+// without keycloak
+
+//     private final TokenProvider tokenProvider;
+
+// @Bean
+//  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//     http
+//         .csrf(csrf -> csrf.disable())
+//         .sessionManagement(session -> 
+//             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//         )
+//         .addFilterBefore(new JWTFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+//         .authorizeHttpRequests(auth -> auth
+//             .requestMatchers("/api/users/register", "/api/users/create", "/api/users/login").permitAll()
+//             .anyRequest().authenticated()
+//         );
+
+//     return http.build();
+// }
+
+//     @Bean
+//     PasswordEncoder defaultPasswordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
+
+//     @Bean
+//     AuthenticationManager defaultAuthenticationManager(AuthenticationConfiguration config) throws Exception {
+//         return config.getAuthenticationManager();
+//     }
 }
