1db04fbe78b8cf06727919fabe0cfbe605a1ae27
kamal shekho <k.shekho@yahoo.com>
2025-09-01 10:37:29 +0200

    jwt converter

--- src/main/java/com/springmvcproject/stickynotes/config/SecurityConfig.java
+++ src/main/java/com/springmvcproject/stickynotes/config/SecurityConfig.java
@@ -3,24 +3,21 @@ package com.springmvcproject.stickynotes.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-// @RequiredArgsConstructor(onConstructor_ = { @Autowired })
+@RequiredArgsConstructor(onConstructor_ = { @Autowired })
 public class SecurityConfig {
+
+        private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
      SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,9 +27,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests -> requests
                         .anyRequest()
                         .authenticated());
-       http
+        http
                 .oauth2ResourceServer(server -> server
-                        .jwt());
+                        .jwt()
+                        .jwtAuthenticationConverter(jwtAuthConverter));
 
         http
                 .sessionManagement(management -> management
